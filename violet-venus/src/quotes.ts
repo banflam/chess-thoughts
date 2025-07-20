c8e45f2e9a7f377f819babcdf8d21e4222e373a0
banflam <rishiagrawal2002@gmail.com>
2025-07-19 23:52:24 -0400

    completed refactoring of the quotes array

--- violet-venus/src/quotes.ts
+++ violet-venus/src/quotes.ts
@@ -1,208 +1,235 @@
 type Quote = {
-	text: string;
-	author: string;
+  text: string;
+  author: string;
 };
 
 const quotes: Quote[] = [
-  [
-    "A thorough understanding of the typical mating continuations makes the most complicated sacrificial combinations leading up to them not only not difficult, but almost a matter of course.",
-    "Siegbert Tarrasch",
-  ],
+  {
+    text: "A thorough understanding of the typical mating continuations makes the most complicated sacrificial combinations leading up to them not only not difficult, but almost a matter of course.",
+    author: "Siegbert Tarrasch",
+  },
 
-  [
-    "Personally, I rather look forward to a computer program winning the world Chess Championship. Humanity needs a lesson in humility.",
-    "Richard Dawkings",
-  ],
+  {
+    text: "Personally, I rather look forward to a computer program winning the world Chess Championship. Humanity needs a lesson in humility.",
+    author: "Richard Dawkings",
+  },
 
-  [
-    "Chess is a foolish expedient for making idle people believe they are doing something very clever when they are only wasting their time.",
-    "George Bernard Shaw",
-  ],
+  {
+    text: "Chess is a foolish expedient for making idle people believe they are doing something very clever when they are only wasting their time.",
+    author: "George Bernard Shaw",
+  },
 
-  [
-    "Mistrust is the most necessary characteristic of the chess player.",
-    "Siegbert Tarrasch",
-  ],
+  {
+    text: "Mistrust is the most necessary characteristic of the chess player.",
+    author: "Siegbert Tarrasch",
+  },
 
-  ["All Chess players should have a hobby.", "Savielly Tartakower"],
+  {
+    text: "All Chess players should have a hobby.",
+    author: "Savielly Tartakower",
+  },
 
-  [
-    "It is the aim of the modern school, not to treat every position according to one general law, but according to the principle inherent in the position",
-    "Richard Reti",
-  ],
+  {
+    text: "It is the aim of the modern school, not to treat every position according to one general law, but according to the principle inherent in the position",
+    author: "Richard Reti",
+  },
 
-  ["The only thing chess players have in common is chess.", "Lodewijk Prins"],
+  {
+    text: "The only thing chess players have in common is chess.",
+    author: "Lodewijk Prins",
+  },
 
-  [
-    "We must make sure that Chess will not be like a dead language, very interesting, but for a very small group.",
-    "Sytze Faber",
-  ],
+  {
+    text: "We must make sure that Chess will not be like a dead language, very interesting, but for a very small group.",
+    author: "Sytze Faber",
+  },
 
-  [
-    "Modern Chess is too much concerned with things like Pawn structure. Forget it, Checkmate ends the game.",
-    "Nigel Short",
-  ],
+  {
+    text: "Modern Chess is too much concerned with things like Pawn structure. Forget it, Checkmate ends the game.",
+    author: "Nigel Short",
+  },
 
-  [
-    "The ability to play chess is the sign of a gentleman. The ability to play chess well is the sign of a wasted life.",
-    "Paul Morphy",
-  ],
+  {
+    text: "The ability to play chess is the sign of a gentleman. The ability to play chess well is the sign of a wasted life.",
+    author: "Paul Morphy",
+  },
 
-  [
-    "Chess is a struggle between my desire not to think and my desire not to lose.",
-    "Jan Gustafsson",
-  ],
+  {
+    text: "Chess is a struggle between my desire not to think and my desire not to lose.",
+    author: "Jan Gustafsson",
+  },
 
-  [
-    "You must take your opponent into a deep, dark forest where 2+2 is 5 and the path leading out is only wide enough for one.",
-    "Mikhail Tal",
-  ],
+  {
+    text: "You must take your opponent into a deep, dark forest where 2+2 is 5 and the path leading out is only wide enough for one.",
+    author: "Mikhail Tal",
+  },
 
-  [
-    "Play the opening like a book, the middle game like a magician, and the endgame like a machine.",
-    "Rudolf Spielmann",
-  ],
+  {
+    text: "Play the opening like a book, the middle game like a magician, and the endgame like a machine.",
+    author: "Rudolf Spielmann",
+  },
 
-  ["Chess demands total concentration.", "Bobby Fischer"],
+  { text: "Chess demands total concentration.", author: "Bobby Fischer" },
 
-  ["Pawns are the soul of chess.", "Philidor"],
+  { text: "Pawns are the soul of chess.", author: "Philidor" },
 
-  ["All I want to do, ever, is just play chess.", "Bobby Fischer"],
+  {
+    text: "All I want to do, ever, is just play chess.",
+    author: "Bobby Fischer",
+  },
 
-  [
-    "Adequate compensation for a sacrifice is having a sound combination leading to a winning position; adequate compensation for a blunder is having your opponent snatch defeat from the jaws of victory.",
-    "Bruce A. Moon",
-  ],
+  {
+    text: "Adequate compensation for a sacrifice is having a sound combination leading to a winning position; adequate compensation for a blunder is having your opponent snatch defeat from the jaws of victory.",
+    author: "Bruce A. Moon",
+  },
 
-  [
-    "Only the player with the initiative has the right to attack.",
-    "Wilhelm Steinitz",
-  ],
+  {
+    text: "Only the player with the initiative has the right to attack.",
+    author: "Wilhelm Steinitz",
+  },
 
-  ["I like the moment when I break a man’s ego.", "Bobby Fischer"],
+  {
+    text: "I like the moment when I break a man’s ego.",
+    author: "Bobby Fischer",
+  },
 
-  [
-    "I got locked up when I was living in Maryland, selling drugs and stuff. But that doesn't mean that the game was over. That's how I equate chess with life.",
-    "Marcel Anderson, chess hustler",
-  ],
+  {
+    text: "I got locked up when I was living in Maryland, selling drugs and stuff. But that doesn't mean that the game was over. That's how I equate chess with life.",
+    author: "Marcel Anderson, chess hustler",
+  },
 
-  [
-    "My girlfriend said, ‘It’s either me or the chess pieces, Marcel.’... I looked at my chess pieces, I looked at her again. And the more I'm looking at her, the more I can see how toxic she is. The more I look at my chess pieces, I can see lots of solitude. I say, “You know what? I'll talk to you later.” I picked up my pieces, I left.",
-    "Marcel Anderson, chess hustler",
-  ],
+  {
+    text: "My girlfriend said, ‘It’s either me or the chess pieces, Marcel.’... I looked at my chess pieces, I looked at her again. And the more I'm looking at her, the more I can see how toxic she is. The more I look at my chess pieces, I can see lots of solitude. I say, “You know what? I'll talk to you later.” I picked up my pieces, I left.",
+    author: "Marcel Anderson, chess hustler",
+  },
 
-  [
-    "I threw my phone off the Brooklyn Bridge. You need to give the game your full attention.",
-    "Ambakisye Osayaba, chess hustler",
-  ],
+  {
+    text: "I threw my phone off the Brooklyn Bridge. You need to give the game your full attention.",
+    author: "Ambakisye Osayaba, chess hustler",
+  },
 
-  [
-    "Alekhine is a poet who creates a work of art out of something that would hardly inspire another man to send home a picture post card.",
-    "Max Euwe",
-  ],
+  {
+    text: "Alekhine is a poet who creates a work of art out of something that would hardly inspire another man to send home a picture post card.",
+    author: "Max Euwe",
+  },
 
-  [
-    "During a chess competition a Chessmaster should be a combination of a beast of prey and a monk.",
-    "Alexander Alekhine",
-  ],
+  {
+    text: "During a chess competition a Chessmaster should be a combination of a beast of prey and a monk.",
+    author: "Alexander Alekhine",
+  },
 
-  ["Chess is above all, a fight!", "Emmanuel Lasker"],
+  { text: "Chess is above all, a fight!", author: "Emmanuel Lasker" },
 
-  [
-    "The passed Pawn is a criminal, who should be kept under lock and key. Mild measures, such as police surveillance, are not sufficient.",
-    "Aaron Nimzovich",
-  ],
+  {
+    text: "The passed Pawn is a criminal, who should be kept under lock and key. Mild measures, such as police surveillance, are not sufficient.",
+    author: "Aaron Nimzovich",
+  },
 
-  ["Life is too short for chess.", "Henry James Byron"],
+  { text: "Life is too short for chess.", author: "Henry James Byron" },
 
-  [
-    "Let the chessboard supercede the card table, and a great improvement will be visible in the morals of the community.",
-    "Paul Morphy",
-  ],
+  {
+    text: "Let the chessboard supercede the card table, and a great improvement will be visible in the morals of the community.",
+    author: "Paul Morphy",
+  },
 
-  [
-    "My life was in crisis. All my values were becoming meaningless. I was discovering that my chosen profession was empty, foolish, as useless as—as playing chess.",
-    "Robert Silverberg",
-  ],
+  {
+    text: "My life was in crisis. All my values were becoming meaningless. I was discovering that my chosen profession was empty, foolish, as useless as—as playing chess.",
+    author: "Robert Silverberg",
+  },
 
-  [
-    "Shall we play Bezique? Or Nightjack? I’ve cards, or chess if you prefer, but I’ve always found chess to be a bit too much like real life to provide much enjoyment as a game.",
-    "Catherynne M. Valente",
-  ],
+  {
+    text: "Shall we play Bezique? Or Nightjack? I’ve cards, or chess if you prefer, but I’ve always found chess to be a bit too much like real life to provide much enjoyment as a game.",
+    author: "Catherynne M. Valente",
+  },
 
-  ["I’d rather have a pawn than a finger.", "Reuben Fine"],
-  ["When in doubt, play chess.", "Michael Tevis"],
-  [
-    "The winner of the game is the person who makes the next-to-last mistake.",
-    "Savielly Tartakower",
-  ],
-  ["The mistakes are all there waiting to be made.", "Savielly Tartakower"],
-  [
-    "No price is too great for the scalp of the enemy King.",
-    "Alexander Koblencs",
-  ],
-  ["The threat is stronger than the execution.", "Aron Nimzowitsch"],
-  [
-    "I found that I was frequently underestimating the bishop pair as a long-term asset.",
-    "Sam Shankland",
-  ],
-  [
-    "Even the laziest King flees wildly in the face of a double check!",
-    "Aron Nimzowitsch",
-  ],
-  [
-    "A win by an unsound combination, however showy, fills me with artistic horror.",
-    "Wilhelm Steinitz",
-  ],
-  [
-    "One doesn't have to play well, it's enough to play better than your opponent.",
-    "Siegbert Tarrasch",
-  ],
-  [
-    "You must take your opponent into a deep dark forest where 2+2=5, and the path leading out is only wide enough for one.",
-    "Mikhail Tal",
-  ],
+  { text: "I’d rather have a pawn than a finger.", author: "Reuben Fine" },
+  { text: "When in doubt, play chess.", author: "Michael Tevis" },
+  {
+    text: "The winner of the game is the person who makes the next-to-last mistake.",
+    author: "Savielly Tartakower",
+  },
+  {
+    text: "The mistakes are all there waiting to be made.",
+    author: "Savielly Tartakower",
+  },
+  {
+    text: "No price is too great for the scalp of the enemy King.",
+    author: "Alexander Koblencs",
+  },
+  {
+    text: "The threat is stronger than the execution.",
+    author: "Aron Nimzowitsch",
+  },
+  {
+    text: "I found that I was frequently underestimating the bishop pair as a long-term asset.",
+    author: "Sam Shankland",
+  },
+  {
+    text: "Even the laziest King flees wildly in the face of a double check!",
+    author: "Aron Nimzowitsch",
+  },
+  {
+    text: "A win by an unsound combination, however showy, fills me with artistic horror.",
+    author: "Wilhelm Steinitz",
+  },
+  {
+    text: "One doesn't have to play well, it's enough to play better than your opponent.",
+    author: "Siegbert Tarrasch",
+  },
+  {
+    text: "You must take your opponent into a deep dark forest where 2+2=5, and the path leading out is only wide enough for one.",
+    author: "Mikhail Tal",
+  },
 
-  [
-    "It is to Petrosian's advantage that his opponents never know when he is suddenly going to play like Mikhail Tal.",
-    "Boris Spassky",
-  ],
+  {
+    text: "It is to Petrosian's advantage that his opponents never know when he is suddenly going to play like Mikhail Tal.",
+    author: "Boris Spassky",
+  },
 
-  [
-    "Tactics is knowing what to do when there is something to do. Strategy is knowing what to do when there is nothing to do.",
-    "Savielly Tartakower",
-  ],
+  {
+    text: "Tactics is knowing what to do when there is something to do. Strategy is knowing what to do when there is nothing to do.",
+    author: "Savielly Tartakower",
+  },
 
-  ["The move is there, but you must see it.", "Savielly Tartakower"],
+  {
+    text: "The move is there, but you must see it.",
+    author: "Savielly Tartakower",
+  },
 
-  [
-    "That which has reached us from the discoveries of their clear thinking and the marvels of their inventions is the (game) of chess. The Indians have, in the construction of its cells, its double numbers, its symbols and secrets, reached the forefront of knowledge.",
-    "Said al-Andalusi",
-  ],
+  {
+    text: "That which has reached us from the discoveries of their clear thinking and the marvels of their inventions is the (game) of chess. The Indians have, in the construction of its cells, its double numbers, its symbols and secrets, reached the forefront of knowledge.",
+    author: "Said al-Andalusi",
+  },
 
-  ["It is eminently and emphatically the philosopher's game.", "Paul Morphy"],
+  {
+    text: "It is eminently and emphatically the philosopher's game.",
+    author: "Paul Morphy",
+  },
 
-  ["The checkmate is the 'knockout' of chess.", "Bobby Fischer"],
+  {
+    text: "The checkmate is the 'knockout' of chess.",
+    author: "Bobby Fischer",
+  },
 
-  [
-    "Chess games are being played everywhere -- on benches and tables in the park, at Chess clubs, YMCAs, high schools, colleges, army posts, prisons. Even by mail. You shouldn't have any trouble getting a game.",
-    "Bobby Fischer",
-  ],
+  {
+    text: "Chess games are being played everywhere -- on benches and tables in the park, at Chess clubs, YMCAs, high schools, colleges, army posts, prisons. Even by mail. You shouldn't have any trouble getting a game.",
+    author: "Bobby Fischer",
+  },
 
-  ["A good player is always lucky.", "Jose Capablanca"],
+  { text: "A good player is always lucky.", author: "Jose Capablanca" },
 
-  [
-    "It doesn’t matter how strong a player you are, if you fail to register some development in the opening, then you are asking for trouble.",
-    "John Emms",
-  ],
+  {
+    text: "It doesn’t matter how strong a player you are, if you fail to register some development in the opening, then you are asking for trouble.",
+    author: "John Emms",
+  },
 
-  [
-    "For in the idea of chess and the development of the chess mind we have a picture of the intellectual struggle of mankind.",
-    "Richard Réti",
-  ],
+  {
+    text: "For in the idea of chess and the development of the chess mind we have a picture of the intellectual struggle of mankind.",
+    author: "Richard Réti",
+  },
 
-  [
-    "In chess, as a purely intellectual game, where randomness is excluded, - for someone to play against himself is absurd ... It is as paradoxical, as attempting to jump over his own shadow.",
-    "Stephan Zweig",
-  ],
+  {
+    text: "In chess, as a purely intellectual game, where randomness is excluded, - for someone to play against himself is absurd ... It is as paradoxical, as attempting to jump over his own shadow.",
+    author: "Stephan Zweig",
+  },
 ];
